35e766adbf0799e65df2d2e50036571d3f3a679d
Gjindu <Gajindugunarathna@gmail.com>
2025-07-05 05:16:45 +0530

    updated serivce cenetr

--- src/main/java/com/example/serviceapplication/ServiceapplicationApplication.java
+++ src/main/java/com/example/serviceapplication/ServiceapplicationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceapplicationApplication {
+public class  ServiceapplicationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceapplicationApplication.class, args);
